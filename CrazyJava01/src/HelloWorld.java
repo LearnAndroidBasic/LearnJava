d15f6502973743f0bbaea805992a69d9db15e05a
YangXiufan <yxf2468@163.com>
2016-10-26 23:26:34 +0800

    static and non static

--- CrazyJava01/src/HelloWorld.java
+++ CrazyJava01/src/HelloWorld.java
@@ -3,6 +3,20 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("Hello");
+        HelloWorld.helloworld();
+        HelloWorld person1 = new HelloWorld();
+        person1.commonhelloworld();
+        HelloWorld person2 = new HelloWorld();
+        person2.commonhelloworld();
+
+    }
+
+    public static void helloworld() {
+        System.out.println(1 + 1);
+    }
+
+    public void commonhelloworld() {
+        System.out.println(1 * 3);
     }
 }
